33997caf172dcb1b49d38cf4045694d78034d4a2
Jack Kennedy <puttums6790@gmail.com>
2018-05-11 07:26:47 -0500

    mongo url

--- server/db/connect.js
+++ server/db/connect.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/relacks')
+mongoose.connect('mongodb://heroku_dqfv1cvb:lecep8u2qv0ri09dsjddu56pf2@ds119449.mlab.com:19449/heroku_dqfv1cvb')
 
 module.exports = mongoose
